c6bb56f41d6291ea45de3061474c5bf51a43c86b
zero <zero@localhost.localdomain>
2023-05-15 00:28:20 +0800

    collections new

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -10,4 +10,11 @@ fn main() {
         Some(third) => println!("the third item is: {}", third),
         None => println!("There is no third element."),
     }
+
+    let mut vec_arr = vec![1, 2, 3, 4, 5, 6];
+    // let mut first = &mut vec_arr[0];
+    let first = vec_arr[0];
+    vec_arr.push(7);
+    println!("The first element is: {first}");
+    println!("The vector array is: {:?}", vec_arr);
 }
